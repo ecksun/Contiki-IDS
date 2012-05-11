b4031ff5faaae1532346288999bc066232479127
Linus Wallgren <linus@sics.se>
2012-05-11 16:08:27 +0200

    Implemented a mapper which maps the network and its parents
    Crashes when trying to map the entire network, unknown cause (probably
    out of memory)

--- examples/ipv6/rpl-collect/mapper.c
+++ examples/ipv6/rpl-collect/mapper.c
@@ -56,6 +56,94 @@ static struct etimer map_timer;
 PROCESS(ids_server, "IDS Server");
 AUTOSTART_PROCESSES(&ids_server);
 
+struct Node {
+  uip_ipaddr_t id;
+  struct Node * parent;
+  struct Node * children[NETWORK_NODES/4];
+  int child;
+};
+
+static struct Node network[NETWORK_NODES];
+static int node_index = 0;
+
+/**
+ * Find the node specified or initialize a new one if it does not already exist
+ *
+ * @return Returns NULL if we run out of memory
+ */
+struct Node * find_node(uip_ipaddr_t * ip) {
+  int i;
+  for (i = 0; i < node_index; ++i) {
+    if (uip_ipaddr_cmp(ip, &network[i].id)) {
+      return &network[i];
+    }
+  }
+  if (node_index >= NETWORK_NODES-1) {
+    printf("OUT OF MEMORY!\n");
+    return NULL;
+  }
+  else { // We found nothing, initialize a new one
+    printf("Creating new node: ");
+    uip_debug_ipaddr_print(ip);
+    printf("\n");
+    memcpy(&network[node_index].id, ip, sizeof(network[node_index].id));
+    return &network[node_index++];
+  }
+}
+
+void print_subtree(struct Node * node, int depth) {
+  int i;
+  printf("%*s", depth*2, "");
+  if (depth > 8) { // uggly hack to avoid loops
+    printf("...\n");
+    return;
+  }
+
+  uip_debug_ipaddr_print(&node->id);
+  printf("\n");
+  for (i = 0; i < node->child; ++i) print_subtree(node->children[i], depth+1);
+}
+
+void print_graph() {
+  printf("Network graph:\n\n");
+  print_subtree(&network[0], 0);
+  printf("-----------------------\n");
+}
+
+void tcpip_handler() {
+  if (!uip_newdata())
+    return;
+
+  uint8_t *appdata;
+  uip_ipaddr_t src_ip, parent_ip;
+  struct Node *id, *parent;
+
+  appdata = (uint8_t *)uip_appdata;
+  memcpy(&src_ip, appdata, sizeof(src_ip));
+  printf("Source IP: ");
+  uip_debug_ipaddr_print(&src_ip);
+  printf("\n");
+  id = find_node(&src_ip);
+  if (id == NULL)
+    return;
+
+  appdata += sizeof(uint8_t) + 2*sizeof(uip_ipaddr_t);
+  memcpy(&parent_ip, appdata, sizeof(parent_ip));
+  parent = find_node(&parent_ip);
+  if (parent == NULL)
+    return;
+
+  id->parent = parent;
+  parent->children[parent->child++] = id;
+
+  printf("parent: ");
+  uip_debug_ipaddr_print(&parent_ip);
+  printf("\n");
+
+  print_graph();
+
+}
+
 void map_network() {
   for (; working_host < UIP_DS6_ROUTE_NB && !uip_ds6_routing_table[working_host].isused; ++working_host);
   if (!uip_ds6_routing_table[working_host].isused) {
@@ -70,7 +158,7 @@ void map_network() {
   printf("sending data to ");
   uip_debug_ipaddr_print(&uip_ds6_routing_table[working_host].ipaddr);
   printf("\n");
-  uip_udp_packet_sendto(ids_conn, data, sizeof(data), &uip_ds6_routing_table[working_host++].ipaddr, UIP_HTONS(MAPPER_CLIENT_PORT)); 
+  uip_udp_packet_sendto(ids_conn, data, sizeof(data), &uip_ds6_routing_table[working_host++].ipaddr, UIP_HTONS(MAPPER_CLIENT_PORT));
   if (working_host >= UIP_DS6_ROUTE_NB-1) {
     working_host = 0;
     etimer_reset(&map_timer);
@@ -89,6 +177,7 @@ PROCESS_THREAD(ids_server, ev, data)
   PROCESS_PAUSE();
 
   memset(host, 0, sizeof(host));
+  memset(network, 0, sizeof(network));
 
   PRINTF("IDS Server, compile time: %s\n", __TIME__);
 
@@ -124,14 +213,17 @@ PROCESS_THREAD(ids_server, ev, data)
   etimer_set(&timer, CLOCK_SECOND);
   etimer_set(&map_timer, 10*CLOCK_SECOND);
 
+  // Add this node (root node) to the network graph
+  memcpy(&network[0].id, &uip_ds6_get_link_local(ADDR_PREFERRED)->ipaddr, sizeof(network[0].id));
+  ++node_index;
+
   while(1) {
+    // printf("snurrar\n");
     PROCESS_YIELD();
     if(ev == tcpip_event) {
-      printf("tcpip event\n");
-      // tcpip_handler();
-    } 
+      tcpip_handler();
+    }
     else if (etimer_expired(&map_timer)) { // && etimer_expired(&timer)) {
-      printf("pre working_host = %d\n", working_host);
       // Map the next DAG.
       if (working_host == 0) {
         for (; mapper_instance < RPL_MAX_INSTANCES; ++mapper_instance) {
@@ -154,11 +246,8 @@ PROCESS_THREAD(ids_server, ev, data)
       }
 
 found_network:
-      printf("mapper_dag = %d, mapper_instance = %d, working_host = %d\n", mapper_dag, mapper_instance, working_host);
-
       map_network();
       etimer_reset(&timer);
-      printf("post working_host = %d\n", working_host);
     }
   }
 
